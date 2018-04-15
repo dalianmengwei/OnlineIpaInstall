6d3d35aa7d125db3c224e8a9371a2c4ebb3c1c66
mw <dalianmengwei@sohu.com>
2018-04-15 16:08:49 +0800

    no message

--- upload.py
+++ upload.py
@@ -15,14 +15,14 @@ import shutil
 import subprocess
 from qiniu import Auth, put_file, etag
 import qiniu.config
-from jinja2 import Environment, FileSystemLoader
+from jinja2 import Environment, FileSystemLoader 
 
 # Read user config file
 script_dir = os.path.dirname(os.path.realpath(__file__))
 user_config = os.path.join(script_dir, 'config', 'user.config')
 
 with open(user_config, 'r') as read:
-    config = json.load(read)
+    config = json.load(read)  
 
 q = Auth(config['access_key'], config['secret_key'])
 
@@ -33,7 +33,7 @@ def render_and_write(template_file, path, context):
     if not rendered_content:
         print("Render returned None - skipping '%s'" % path)
         return
-    
+
     with open(path, 'wb') as out:
         out.write(rendered_content.encode('utf-8').strip())
 
@@ -54,15 +54,15 @@ def get_file_size(filePath):
 class IPAParser(object):
     
     plist_file_rx = re.compile(r'Payload/.+?\.app/Info.plist$')
-    
+
     def __init__(self, ipa_file_path):
         self.ipa_file_path = ipa_file_path
         self.zip_obj = zipfile.ZipFile(self.ipa_file_path, 'r')
-    
+
     def get_files_from_ipa(self, file_regx):
         filenames = [file for file in self.zip_obj.namelist() if file_regx.search(file)]
         return filenames
-    
+
     def get_info_plist_data(self):
         info_plists = self.get_files_from_ipa(self.plist_file_rx)
         if len(info_plists):
@@ -71,7 +71,7 @@ class IPAParser(object):
             plist_root = plistlib.loads(info_plist_data)
             return plist_root
         return None
-    
+
     def main_icon_name(self):
         result = []
         icon_name = None
@@ -80,10 +80,10 @@ class IPAParser(object):
         and 'CFBundlePrimaryIcon' in plist_root['CFBundleIcons'] \
         and 'CFBundleIconFiles' in plist_root['CFBundleIcons']['CFBundlePrimaryIcon']:
             result =  plist_root['CFBundleIcons']['CFBundlePrimaryIcon']['CFBundleIconFiles']
-        
+
         if not len(result) and 'CFBundleIconFiles' in plist_root :
             result = plist_root['CFBundleIconFiles']
-        
+
         if len(result):
             filter_result = list(filter(lambda x: '@2x' in x, result))
             if not len(filter_result):
@@ -94,24 +94,24 @@ class IPAParser(object):
                 icon_name = filter_result[0]
         else:
             icon_name = plist_root['CFBundleIconFile']
-        
+
         if icon_name and os.path.splitext(icon_name)[1] == '':
             icon_name += '.png'
-        
+
         return icon_name
     
     def extra_file_to_path(self, filename, path):
         if not filename:
             return None
-        
+
         filter_list = list(filter(lambda x : x.endswith(filename) ,self.zip_obj.namelist()))
         if len(filter_list):
             file_path = filter_list[0]
             self.zip_obj.extractall(path, members=[file_path])
             return os.path.join(path, file_path)
-        
+            
         return None
-    
+
     def is_valid_zip_file(self):
         return zipfile.is_zipfile(self.ipa_file_path)
 
@@ -127,23 +127,23 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='ipa-upload-script (by AloneMonkey v1.0)')
     parser.add_argument('target', nargs='?', help='The path of target ipa file')
     args = parser.parse_args()
-    
+
     exit_code = 0
     failed_code = -1
     errors = []
-    
+
     if not len(sys.argv[1:]):
         parser.print_help()
         sys.exit(exit_code)
 
-    ipa_file_path = args.target
+    ipa_file_path = args.target 
 
-if not os.path.exists(ipa_file_path):
-    print("[%s] is not existed!" % ipa_file_path)
-    exit(failed_code)
-    
+    if not os.path.exists(ipa_file_path):
+        print("[%s] is not existed!" % ipa_file_path)
+        exit(failed_code)
+
     ipa_parser = IPAParser(ipa_file_path)
-    
+
     if not ipa_parser.is_valid_zip_file():
         print('[%s] is not a valid zip file' % ipa_file_path)
         sys.exit(failed_code)
@@ -155,54 +155,54 @@ if not os.path.exists(ipa_file_path):
     display_name = plist_root.get('CFBundleDisplayName') or plist_root.get('CFBundleName')
     executable_name = plist_root.get('CFBundleExecutable')
 
-# upload ipa file
-upload_ipa_url = upload_file_to_qiniu(ipa_file_path, executable_name + ".ipa")
+    # upload ipa file
+    upload_ipa_url = upload_file_to_qiniu(ipa_file_path, executable_name + ".ipa")
 
-icon_name = ipa_parser.main_icon_name()
+    icon_name = ipa_parser.main_icon_name() 
 
-upload_icon_url = 'http://o9slcszsf.bkt.clouddn.com/largeImage.png'
-    
+    upload_icon_url = 'http://o9slcszsf.bkt.clouddn.com/largeImage.png'
+
     local_icon_path = ipa_parser.extra_file_to_path(icon_name, os.path.join(script_dir, 'icons', executable_name))
-    
+
     if local_icon_path:
         pngcrush_args = ('/Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/usr/bin/pngcrush', '-q', '-e', '.fixed', '-revert-iphone-optimizations', local_icon_path)
         try:
             subprocess.check_call(pngcrush_args)
         except subprocess.CalledProcessError as err:
             print(err)
-        shutil.move(os.path.splitext(local_icon_path)[0] + '.fixed', local_icon_path)
+        shutil.move(os.path.splitext(local_icon_path)[0] + '.fixed', local_icon_path)   
         upload_icon_url = upload_file_to_qiniu(local_icon_path, executable_name + '-' + icon_name)
 
     plist_name = executable_name + '.plist'
     html_name = executable_name + '.html'
 
-write_plist_path = os.path.join(script_dir, 'plists', plist_name)
-write_html_path = os.path.join(script_dir, 'htmls', html_name)
+    write_plist_path = os.path.join(script_dir, 'plists', plist_name)
+    write_html_path = os.path.join(script_dir, 'htmls', html_name)
 
-render_and_write(os.path.join(script_dir, 'template.plist'), write_plist_path,
-                 {
-                 'ipa' : upload_ipa_url,
-                 'large' : upload_icon_url,
-                 'small' : upload_icon_url,
-                 'bundleid' : bundle_id,
-                 'version' : bundle_version,
-                 'name' : display_name
-                 })
+    render_and_write(os.path.join(script_dir, 'template.plist'), write_plist_path, 
+        {
+            'ipa' : upload_ipa_url,
+            'large' : upload_icon_url,
+            'small' : upload_icon_url,
+            'bundleid' : bundle_id,
+            'version' : bundle_version,
+            'name' : display_name
+        })
 
-render_and_write(os.path.join(script_dir, 'template.html'), write_html_path,
-                 {
-                 'icon' : upload_icon_url,
-                 'name' : display_name,
-                 'version' : bundle_version,
-                 'build' : bundle_version_build,
-                 'size' : get_file_size(ipa_file_path),
-                 'plisturl' : os.path.join(config['repo_url'], 'plists', plist_name)
-                 })
+    render_and_write(os.path.join(script_dir, 'template.html'), write_html_path,
+        {
+          'icon' : upload_icon_url,
+          'name' : display_name,
+          'version' : bundle_version,
+          'build' : bundle_version_build,
+          'size' : get_file_size(ipa_file_path),
+          'plisturl' : os.path.join(config['repo_url'], 'plists', plist_name)
+        })
 
-html_url = upload_file_to_qiniu(write_html_path, html_name)
+    html_url = upload_file_to_qiniu(write_html_path, html_name)
 
-print("Please push local branch to remote, Then open %s install" % html_url)
+    print("Please push local branch to remote, Then open %s install" % html_url)
 
+    
 
-
-
+    
